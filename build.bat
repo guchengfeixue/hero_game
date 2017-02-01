db054ccbf14cb33d03896d0a5105caef295da4dd
guchengfeixue <guchengfeixue@gmail.com>
2017-02-01 18:15:05 +0800

    Hero Game Day 002 创建windows窗口

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir ..\cmd_build
 pushd ..\cmd_build
-cl -Zi ..\herogame\win32_herogame.cpp user32.lib
+cl -Zi ..\herogame\win32_herogame.cpp user32.lib Gdi32.lib
 popd
--- herogame/win32_herogame.cpp
+++ herogame/win32_herogame.cpp
@@ -1,7 +1,114 @@
 #include <windows.h>
 
+LRESULT CALLBACK MainWindowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	LRESULT Result = 0;
+
+	switch (uMsg)
+	{
+		case WM_SIZE:
+		{
+			OutputDebugStringA("WM_SIZE\n");
+
+		}break;
+
+		case WM_DESTROY:
+		{
+			OutputDebugStringA("WM_DESTROY\n");
+		}break;
+
+		case WM_CLOSE:
+		{
+			
+		}break;
+
+		case WM_ACTIVATEAPP:
+		{
+			
+		}break;
+
+		case WM_PAINT:
+		{
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(hwnd, &Paint);
+
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			static DWORD Operation = WHITENESS;
+			PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+
+			if (Operation == WHITENESS)
+				Operation = BLACKNESS;
+			else
+				Operation = WHITENESS;	
+
+			EndPaint(hwnd, &Paint);
+		}break;
+
+		default:
+		{
+			Result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}break;
+	}
+	return (Result);
+}
+
+
+
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int)
 {
-	MessageBox(0, "This is Hero Game.", "Hero Game", MB_OK | MB_ICONINFORMATION);
-	return 0;
+	//MessageBoxA(0, "This is Hero Game.", "Hero Game", MB_OK | MB_ICONINFORMATION);
+	WNDCLASS WindowClass = {};
+
+	//TODO : check CS_HREDRAW CS_VREDRAW OWNDC still matter
+	//WindowClass.style			= CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	WindowClass.lpfnWndProc		= MainWindowCallback;
+	//WindowClass.hInstance		= hInstance;
+	WindowClass.hInstance		= GetModuleHandle(0);
+	WindowClass.lpszClassName	= "HeroGameWindowClass";
+
+	if (RegisterClassA(&WindowClass))
+	{
+		HWND WindowHandle = CreateWindowExA(
+			0,
+			WindowClass.lpszClassName,
+			"Hero Game",
+			WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0,
+			0,
+			hInstance,
+			0);
+
+		if (WindowHandle)
+		{
+			MSG Message;
+			for (;;)
+			{
+				BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
+				if (MessageResult > 0)
+				{
+					TranslateMessage(&Message);
+					DispatchMessageA(&Message);
+				}
+				else
+					break;
+			}
+		}
+		else
+		{
+			//TODO Logging
+		}
+	}
+	else
+	{
+		//TODO Logging
+	}
+
+	return (0);
 }
