92938ae0b5fdb5993e78b9353ddde9c2a34dbdaa
guchengfeixue <guchengfeixue@gmail.com>
2017-02-28 11:56:01 +0800

    build 脚本增加判断

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-mkdir ..\cmd_build
+if not exist ..\cmd_build mkdir ..\cmd_build
 pushd ..\cmd_build
 cl -DHEROGAME_INTERNAL=1 -DHEROGAME_SLOW=1 -DHEROGAME_WIN32=1 -FC -Zi ..\herogame\win32_herogame.cpp user32.lib Gdi32.lib
 popd
