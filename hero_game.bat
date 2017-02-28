749ceb4e04c74161bd7f82af9c71376be3aeb35c
guchengfeixue <guchengfeixue@gmail.com>
2017-02-28 09:43:25 +0800

    合并两台机器的启动文件

--- hero_game.bat
+++ hero_game.bat
@@ -1,6 +1,7 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-subst w: C:\github\hero_game
+if not exist w: if exist C:\github\hero_game subst w: C:\github\hero_game
+if not exist w: if exist F:\project\github\hero_game subst w: F:\project\github\hero_game
 set path=w;%path%
 w:
 
