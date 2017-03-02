8a1b6a9c77f832286a1e9ac3a5090e5b02451e92
guchengfeixue <guchengfeixue@gmail.com>
2017-03-03 00:02:39 +0800

    调整命令参数

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 if not exist ..\cmd_build mkdir ..\cmd_build
 pushd ..\cmd_build
-cl -MT -nologo -Gm- -GR- -EHa- -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -DHEROGAME_INTERNAL=1 -DHEROGAME_SLOW=1 -DHEROGAME_WIN32=1 -FC -Z7 -Fmwin32_herogame.map ..\herogame\win32_herogame.cpp /link -opt:ref user32.lib Gdi32.lib
+cl -MT -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -DHEROGAME_INTERNAL=1 -DHEROGAME_SLOW=1 -DHEROGAME_WIN32=1 -FC -Z7 -Fmwin32_herogame.map ..\herogame\win32_herogame.cpp /link -opt:ref user32.lib Gdi32.lib
 popd
