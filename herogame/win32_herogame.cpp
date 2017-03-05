ff9306ebeb2f4dd4dc0e284b0f080a7b695985f4
guchengfeixue <guchengfeixue@gmail.com>
2017-03-05 23:18:25 +0800

    添加注释

--- herogame/win32_herogame.cpp
+++ herogame/win32_herogame.cpp
@@ -744,6 +744,22 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 						}
 					}
 
+					/*
+					NOTE:
+					Here is how sound output computation works.
+					We define a safety value that is the number of samples we think our game update loop may vary by(let's say up to 2ms)
+
+					When we wake up to write audio, we will look and see what the play cursor position is and we will forecast ahead where we think the play cursor will
+					be on the next frame boundry.
+
+					We will then look to see if the write cursor is before that by at least our safety value. If it is, the target fill position is that frame boundry
+					plus one frame. This gives us perfect audio sync in the case of a card that has low enough latency.
+
+					If the write cursor is _after_ that safety margin. then we assume we can never sync the audio perfectly, so we will write one frame's worth
+					of audio plus the safety margin's worth of guard samples.
+
+					*/
+
 					DWORD ByteToLock = 0;
 					DWORD TargetCursor = 0;
 					DWORD BytesToWrite = 0;
