e6fcdc2d87cc994b567dd3a8b5bdccc9599b3305
guchengfeixue <guchengfeixue@gmail.com>
2017-07-09 17:37:45 +0800

    hero game day 055 word tile_chunk hash存储

--- herogame/herogame.cpp
+++ herogame/herogame.cpp
@@ -308,7 +308,6 @@ internal uint32 AddWall(game_state *GameState, uint32 AbsTileX, uint32 AbsTileY,
 	EntityLow->Width = EntityLow->Height;
 	EntityLow->Collides = true;
 
-	//ChangeEntityResidence(GameState, EntityIndex, EntityResidence_High);
 	return (EntityIndex);
 }
 
@@ -317,8 +316,8 @@ internal uint32 AddPlayer(game_state *GameState)
 	uint32 EntityIndex = AddLowEntity(GameState, EntityType_Hero);
 	low_entity *EntityLow = GetLowEntity(GameState, EntityIndex);
 
-	EntityLow->P.AbsTileX = 1;
-	EntityLow->P.AbsTileY = 3;
+	EntityLow->P= GameState->CameraP;
+
 	EntityLow->P.Offset_.X = .0f;
 	EntityLow->P.Offset_.Y = .0f;
 	EntityLow->Height = 0.5f;
@@ -478,16 +477,16 @@ internal void SetCamera(game_state *GameState, tile_map_position NewCameraP)
 	v2 EntityOffsetForFrame = -dCameraP.dXY;
 	OffsetAndCheckFrequencyByArea(GameState, EntityOffsetForFrame, CameraBounds);
 
-	uint32 MinTileX = NewCameraP.AbsTileX - TileSpanX / 2;
-	uint32 MinTileY = NewCameraP.AbsTileY - TileSpanY / 2;
-	uint32 MaxTileX = NewCameraP.AbsTileX + TileSpanX / 2;
-	uint32 MaxTileY = NewCameraP.AbsTileY + TileSpanY / 2;
+	int32 MinTileX = NewCameraP.AbsTileX - TileSpanX / 2;
+	int32 MinTileY = NewCameraP.AbsTileY - TileSpanY / 2;
+	int32 MaxTileX = NewCameraP.AbsTileX + TileSpanX / 2;
+	int32 MaxTileY = NewCameraP.AbsTileY + TileSpanY / 2;
 	for (uint32 EntityIndex = 1; EntityIndex < GameState->LowEntityCount; ++EntityIndex)
 	{
 		low_entity *Low = GameState->LowEntities + EntityIndex;
 		if (Low->HighEntityIndex == 0)
 		{
-			if ((Low->P.AbsTileZ == NewCameraP.AbsTileX) &&
+			if ((Low->P.AbsTileZ == NewCameraP.AbsTileZ) &&
 				(Low->P.AbsTileX >= MinTileX) &&
 				(Low->P.AbsTileX < MaxTileX) &&
 				(Low->P.AbsTileY >= MinTileY) &&
@@ -552,31 +551,19 @@ extern "C" GAME_UPDATE_AND_RENDER(GameUpdateAndRender)
 		World->TileMap = PushStruct(&GameState->WorldArena, tile_map);
 		tile_map *TileMap = World->TileMap;
 
-		TileMap->ChunkShift = 4;
-		TileMap->ChunkMask = (1 << TileMap->ChunkShift) - 1;
-		TileMap->ChunkDim = (1 << TileMap->ChunkShift);
-
-		TileMap->TileChunkCountX = 128;
-		TileMap->TileChunkCountY = 128;
-		TileMap->TileChunkCountZ = 2;
-
-		TileMap->TileChunks = PushArray(
-			&GameState->WorldArena, TileMap->TileChunkCountX * TileMap->TileChunkCountY * TileMap->TileChunkCountZ, tile_chunk);
-		TileMap->TileSideInMeters = 1.4f;
+		InitializeTileMap(TileMap, 1.4f);
 
 		uint32 RandomNumberIndex = 0;
 		uint32 TilesPerWidth = 17;
 		uint32 TilesPerHeight = 9;
 
+		uint32 ScreenBaseX = 0;
+		uint32 ScreenBaseY = 0;
+		uint32 ScreenBaseZ = 0;
 
-#if 0
-		uint32 ScreenX = INT32_MAX / 2;
-		uint32 ScreenY = INT32_MAX / 2;
-#else
-		uint32 ScreenX = 0;
-		uint32 ScreenY = 0;
-#endif
-		uint32 AbsTileZ = 0;
+		uint32 ScreenX = ScreenBaseX;
+		uint32 ScreenY = ScreenBaseY;
+		uint32 AbsTileZ = ScreenBaseZ;
 
 		bool32 DoorLeft = false;
 		bool32 DoorRight = false;
@@ -598,7 +585,7 @@ extern "C" GAME_UPDATE_AND_RENDER(GameUpdateAndRender)
 			if (RandomChoice == 2)
 			{
 				CreatedZDoor = true;
-				if (AbsTileZ == 0)
+				if (AbsTileZ == ScreenBaseZ)
 					DoorUp = true;
 				else
 					DoorDown = true;
@@ -655,10 +642,10 @@ extern "C" GAME_UPDATE_AND_RENDER(GameUpdateAndRender)
 			DoorTop = false;
 			if (RandomChoice == 2)
 			{
-				if (AbsTileZ == 0)
-					AbsTileZ = 1;
+				if (AbsTileZ == ScreenBaseZ)
+					AbsTileZ = ScreenBaseZ + 1;
 				else
-					AbsTileZ = 0;
+					AbsTileZ = ScreenBaseZ;
 			}
 			else if (RandomChoice == 1)
 				ScreenX += 1;
@@ -666,8 +653,10 @@ extern "C" GAME_UPDATE_AND_RENDER(GameUpdateAndRender)
 				ScreenY += 1;
 		}
 		tile_map_position NewCameraP = {};
-		NewCameraP.AbsTileX = 17 / 2;
-		NewCameraP.AbsTileY = 9 / 2;
+		NewCameraP.AbsTileX = ScreenBaseX * TilesPerWidth + 17 / 2;
+		NewCameraP.AbsTileY = ScreenBaseY * TilesPerHeight + 9 / 2;
+		NewCameraP.AbsTileZ = ScreenBaseZ;
+
 		SetCamera(GameState, NewCameraP);
 
 		Memory->IsInitialized = true;
--- herogame/herogame_tile.cpp
+++ herogame/herogame_tile.cpp
@@ -1,15 +1,65 @@
-inline tile_chunk *GetTileChunk(tile_map *TileMap, uint32 TileChunkX, uint32 TileChunkY, uint32 TileChunkZ)
+#define TILE_CHUNK_SAFE_MARGIN (INT32_MAX/64)
+#define TILE_CHUNK_UNINITIALIZED INT32_MAX
+
+internal void InitializeTileMap(tile_map *TileMap, real32 TileSideInMeters)
 {
-	tile_chunk *TileChunk = 0;
-	if ((TileChunkX >= 0) && (TileChunkX < TileMap->TileChunkCountX) &&
-		(TileChunkY >= 0) && (TileChunkY < TileMap->TileChunkCountY) &&
-		(TileChunkZ >=0) && (TileChunkZ < TileMap->TileChunkCountZ))
+	TileMap->ChunkShift = 4;
+	TileMap->ChunkMask = (1 << TileMap->ChunkShift) - 1;
+	TileMap->ChunkDim = (1 << TileMap->ChunkShift);
+	TileMap->TileSideInMeters = TileSideInMeters;
+	for (uint32 TileChunkIndex = 0; TileChunkIndex < ArrayCount(TileMap->TileChunkHash); ++TileChunkIndex)
+		TileMap->TileChunkHash[TileChunkIndex].TileChunkX = TILE_CHUNK_UNINITIALIZED;
+}
+
+inline tile_chunk *GetTileChunk(tile_map *TileMap, int32 TileChunkX, int32 TileChunkY, int32 TileChunkZ, memory_arena *Arena=0)
+{
+	Assert(TileChunkX > -TILE_CHUNK_SAFE_MARGIN);
+	Assert(TileChunkY > -TILE_CHUNK_SAFE_MARGIN);
+	Assert(TileChunkZ > -TILE_CHUNK_SAFE_MARGIN);
+	Assert(TileChunkX < TILE_CHUNK_SAFE_MARGIN);
+	Assert(TileChunkX < TILE_CHUNK_SAFE_MARGIN);
+	Assert(TileChunkX < TILE_CHUNK_SAFE_MARGIN);
+
+	uint32 HashValue = 19 * TileChunkX + 7 * TileChunkY + 3 * TileChunkZ;
+	uint32 HashSlot = HashValue & (ArrayCount(TileMap->TileChunkHash) - 1);
+	Assert(HashSlot < ArrayCount(TileMap->TileChunkHash));
+
+	tile_chunk *Chunk = TileMap->TileChunkHash + HashSlot;
+	do
 	{
-		TileChunk = &TileMap->TileChunks[
-			TileChunkZ * TileMap->TileChunkCountY * TileMap->TileChunkCountX +
-			TileChunkY * TileMap->TileChunkCountX + TileChunkX];
-	}
-	return (TileChunk);
+		if ((TileChunkX == Chunk->TileChunkX) &&
+			(TileChunkY == Chunk->TileChunkY) &&
+			(TileChunkZ == Chunk->TileChunkZ))
+			break;
+
+		if (Arena && (Chunk->TileChunkX != TILE_CHUNK_UNINITIALIZED) && (!Chunk->NextInHash))
+		{
+			Chunk->NextInHash = PushStruct(Arena, tile_chunk);
+			Chunk = Chunk->NextInHash;
+			Chunk->TileChunkX = TILE_CHUNK_UNINITIALIZED;
+		}
+
+		if (Arena && (Chunk->TileChunkX == TILE_CHUNK_UNINITIALIZED))
+		{
+			uint32 TileCount = TileMap->ChunkDim * TileMap->ChunkDim;
+
+			Chunk->TileChunkX = TileChunkX;
+			Chunk->TileChunkY = TileChunkY;
+			Chunk->TileChunkZ = TileChunkZ;
+
+			Chunk->Tiles = PushArray(Arena, TileCount, uint32);
+			for (uint32 TileIndex = 0; TileIndex < TileCount; ++TileIndex)
+				Chunk->Tiles[TileIndex] = 1;
+
+			Chunk->NextInHash = 0;
+
+			break;
+		}
+
+		Chunk = Chunk->NextInHash;
+	} while (Chunk);
+
+	return (Chunk);
 }
 
 inline tile_chunk_position GetChunkPositionFor(tile_map *TileMap, uint32 AbsTileX, uint32 AbsTileY, uint32 AbsTileZ)
@@ -25,7 +75,7 @@ inline tile_chunk_position GetChunkPositionFor(tile_map *TileMap, uint32 AbsTile
 	return (Result);
 }
 
-inline uint32 GetTileValueUnchecked(tile_map *TileMap, tile_chunk *TileChunk, uint32 TileX, uint32 TileY)
+inline uint32 GetTileValueUnchecked(tile_map *TileMap, tile_chunk *TileChunk, int32 TileX, int32 TileY)
 {
 	Assert(TileChunk);
 	Assert(TileX < TileMap->ChunkDim);
@@ -72,7 +122,7 @@ internal bool32 IsTileMapPointEmpty(tile_map *TileMap, tile_map_position CanPos)
 	return (Empty);
 }
 
-inline void SetTileValueUnchecked(tile_map *TileMap, tile_chunk *TileChunk, uint32 TileX, uint32 TileY, uint32 TileValue)
+inline void SetTileValueUnchecked(tile_map *TileMap, tile_chunk *TileChunk, int32 TileX, int32 TileY, int32 TileValue)
 {
 	Assert(TileChunk);
 	Assert(TileX < TileMap->ChunkDim);
@@ -91,22 +141,12 @@ inline void SetTileValue(tile_map *TileMap, tile_chunk *TileChunk, uint32 TestTi
 internal void SetTileValue(memory_arena *Arena, tile_map *TileMap, uint32 AbsTileX, uint32 AbsTileY, uint32 AbsTileZ, uint32 TileValue)
 {
 	tile_chunk_position ChunkPos = GetChunkPositionFor(TileMap, AbsTileX, AbsTileY, AbsTileZ);
-	tile_chunk *TileChunk = GetTileChunk(TileMap, ChunkPos.TileChunkX, ChunkPos.TileChunkY, ChunkPos.TileChunkZ);
-	Assert(TileChunk);
-	if (!TileChunk->Tiles)
-	{
-		uint32 TileCount = TileMap->ChunkDim * TileMap->ChunkDim;
-		TileChunk->Tiles = PushArray(Arena, TileCount, uint32);
-		for (uint32 TileIndex = 0; TileIndex < TileCount; ++TileIndex)
-		{
-			TileChunk->Tiles[TileIndex] = 1;
-		}
-	}
+	tile_chunk *TileChunk = GetTileChunk(TileMap, ChunkPos.TileChunkX, ChunkPos.TileChunkY, ChunkPos.TileChunkZ, Arena);
 	SetTileValue(TileMap, TileChunk, ChunkPos.RelTileX, ChunkPos.RelTileY, TileValue);
 }
 
 
-inline void RecanonicalizeCoord(tile_map *TileMap, uint32 *Tile, real32 *TileRel)
+inline void RecanonicalizeCoord(tile_map *TileMap, int32 *Tile, real32 *TileRel)
 {
 	int32 Offset = RoundReal32ToInt32(*TileRel / (real32)TileMap->TileSideInMeters);
 	*Tile += Offset;
