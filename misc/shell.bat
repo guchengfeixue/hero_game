004f12dc0d30ac80ea178434ad102121fa19f10f
guchengfeixue <guchengfeixue@gmail.com>
2018-04-07 22:52:41 +0800

    更新shell文件兼容笔记本

--- misc/shell.bat
+++ misc/shell.bat
@@ -5,7 +5,8 @@ REM  To run this at startup, use this as your shortcut target:
 REM  %windir%\system32\cmd.exe /k F:\project\github\hero_game\misc\shell.bat
 REM
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 if not exist w: if exist C:\github\hero_game subst w: C:\github\hero_game
 if not exist w: if exist F:\project\github\hero_game subst w: F:\project\github\hero_game
 set path=w;%path%
