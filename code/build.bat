810c891756e4d7d051aa53990a1765042307b50d
guchengfeixue <guchengfeixue@gmail.com>
2018-04-09 21:37:24 +0800

    Day 021 - Loading Game Code Dynamically Day 022 - Instantaneous Live Code Editing Day 023 - Looped Live Code Editing Day 024 - Win32 Platform Layer Cleanup Day 025 - Finishing the Win32 Prototyping Layer

--- code/build.bat
+++ code/build.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4456 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7 -Fmwin32_handmade.map
-set CommonLinkerFlags= -opt:ref user32.lib gdi32.lib winmm.lib
+set CommonCompilerFlags=-MTd -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4456 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7
+set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib
 
 REM TODO - can we just build both with one exe?
 
@@ -12,5 +12,7 @@ REM 32-bit build
 REM cl %CommonCompilerFlags% ..\handmade\code\win32_handmade.cpp /link -subsystem:windows,5.1 %CommonLinkerFlags%
 
 REM 64-bit build
-cl %CommonCompilerFlags% ..\code\win32_handmade.cpp /link %CommonLinkerFlags%
+del *.pdb > NUL 2> NUL
+cl %CommonCompilerFlags% ..\code\handmade.cpp -Fmhandmade.map -LD /link -incremental:no -opt:ref -PDB:handmade_%random%.pdb -EXPORT:GameGetSoundSamples -EXPORT:GameUpdateAndRender
+cl %CommonCompilerFlags% ..\code\win32_handmade.cpp -Fmwin32_handmade.map /link %CommonLinkerFlags%
 popd
--- code/win32_handmade.h
+++ code/win32_handmade.h
@@ -32,7 +32,6 @@ struct win32_sound_output
     DWORD SecondaryBufferSize;
     DWORD SafetyBytes;
     real32 tSine;
-    int LatencySampleCount;
     // TODO(casey): Should running sample index be in bytes as well
     // TODO(casey): Math gets simpler if we add a "bytes per second" field?
 };
@@ -47,6 +46,43 @@ struct win32_debug_time_marker
     
     DWORD FlipPlayCursor;
     DWORD FlipWriteCursor;
+};
+
+struct win32_game_code
+{
+    HMODULE GameCodeDLL;
+    FILETIME DLLLastWriteTime;
+
+    // IMPORTANT(casey): Either of the callbacks can be 0!  You must
+    // check before calling.
+    game_update_and_render *UpdateAndRender;
+    game_get_sound_samples *GetSoundSamples;
+
+    bool32 IsValid;
+};
+
+#define WIN32_STATE_FILE_NAME_COUNT MAX_PATH
+struct win32_replay_buffer
+{
+    HANDLE FileHandle;
+    HANDLE MemoryMap;
+    char FileName[WIN32_STATE_FILE_NAME_COUNT];
+    void *MemoryBlock;
+};
+struct win32_state
+{
+    uint64 TotalSize;
+    void *GameMemoryBlock;
+    win32_replay_buffer ReplayBuffers[4];
+    
+    HANDLE RecordingHandle;
+    int InputRecordingIndex;
+
+    HANDLE PlaybackHandle;
+    int InputPlayingIndex;
+    
+    char EXEFileName[WIN32_STATE_FILE_NAME_COUNT];
+    char *OnePastLastEXEFileNameSlash;
 };
 
 #define WIN32_HANDMADE_H
