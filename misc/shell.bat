bb6fb6a18277d22814c1da37d6e9989e29482a66
guchengfeixue <guchengfeixue@gmail.com>
2018-04-13 13:50:04 +0800

    Day 050 - Basic Mikowski-based Collision Detection

--- misc/shell.bat
+++ misc/shell.bat
@@ -6,7 +6,7 @@ REM  %windir%\system32\cmd.exe /k F:\project\github\hero_game\misc\shell.bat
 REM
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+if not exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build" if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 if not exist w: if exist C:\github\hero_game subst w: C:\github\hero_game
 if not exist w: if exist F:\project\github\hero_game subst w: F:\project\github\hero_game
 set path=w;%path%
